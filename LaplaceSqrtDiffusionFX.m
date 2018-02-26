2f86798a616da7eee612c757aeeb03bff210f998
Yimin Zhong <yzhong@utexas.edu>
2018-02-26 17:32:32 -0600

    clean up

--- LaplaceSqrtDiffusionFX.m
+++
@@ -1,7 +0,0 @@
-function [ ret ] = LaplaceSqrtDiffusionFX( x )
-%LAPLACEDIFFUSIONFX calculate \nabla\cdot \nabla Dx.
-%   must modify according to Dx.
-%     ret = (0 * x(1,:) - 0.25 * (0.05)^2) ./(0.1 + 0.05 * x(1,:)).^(3/2);
-ret = 0 * x(1,:);
-end
-
--- NormedGradientDiffusionFX.m
+++
@@ -1,8 +0,0 @@
-function [ ret ] = NormedGradientDiffusionFX( x )
-%GRADIENTDIFFUSION 
-%   must modify according to Dx.
-%     ret = 0.05 + 0 * x(1,:);
-ret = 0 * x(1,:);
-
-end
-
